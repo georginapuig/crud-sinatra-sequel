6d9af76fdefbc56400c88ca166c8980dcd4d5e85
georgina <georginaxpuig@gmail.com>
2022-08-26 02:09:22 -0300

    add get, post, delete

---
+++ myapp.rb
@@ -0,0 +1,53 @@
+# myapp.rb
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require "sequel"
+require "logger"
+
+# connect to an in-memory database
+DB = Sequel.sqlite
+DB.loggers << Logger.new($stdout)
+
+# create an items table
+DB.create_table :items do
+  primary_key :id
+  String :name, unique: true, null: false
+  Float :price, null: false
+end
+
+# create a dataset from the items table
+items = DB[:items]
+
+# populate the table
+items.insert(name: 'abc', price: rand * 100)
+items.insert(name: 'def', price: rand * 100)
+items.insert(name: 'ghi', price: rand * 100)
+
+# print out the number of records
+puts "Item count: #{items.count}"
+
+# print out the average price
+puts "The average price is: #{items.avg(:price)}"
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  name = params[:name]
+  price = params[:price]
+  
+  items.insert(name: name, price: price)
+
+  redirect '/'
+end
+
+delete '/:id' do
+  id = params[:id]
+  # p id
+
+  items.where(id: id).delete
+
+  redirect "/"
+end
+
